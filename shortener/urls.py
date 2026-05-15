b865d81c4e67908a1f007db3c9723490816429c6
Pratheeksha Dhananjaya <pratheekshadhananjaya@Mac.fritz.box>
2026-05-15 20:47:14 +0200

    connected delete functionality to backend

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ path('api/v1/short-codes' , views.shorten_url, name='shorten_url'),
-               path('api/v1/short-codes/<str:short_code>',views.redirect_url, name='redirect_url')]
+               path('api/v1/short-codes/<str:short_code>',views.redirect_url, name='redirect_url'),
+               path('api/v1/short-codes/<str:short_code>/delete', views.delete_url, name='delete_url')]
--- shortener/views.py
+++ shortener/views.py
@@ -36,6 +36,13 @@ def shorten_url(request):
             status=status.HTTP_400_BAD_REQUEST
         )
        
+    existing_url = Url.objects.filter(long_url=long_url).first()
+    if existing_url:
+        return Response({
+            'shortUrl': f'http://127.0.0.1:8000/api/v1/short-codes/{existing_url.short_code}',
+            'longUrl':long_url,
+            'createdAt':existing_url.created_at
+        },status=status.HTTP_200_OK)
     
     short_code = generate_short_code()
     url = Url.objects.create(
@@ -58,4 +65,19 @@ def redirect_url(request,short_code):
         return Response(
             {'message' : 'Short code not found'},
             status = status.HTTP_404_NOT_FOUND
+        )
+    
+@api_view(['DELETE'])
+def delete_url(request,short_code):
+    try:
+        url=Url.objects.get(short_code=short_code)
+        url.delete()
+        return Response(
+            {'message':'URL deleted successfully'},
+            status=status.HTTP_200_OK
+        )
+    except Url.DoesNotExist:
+        return Response(
+            {'message':'Short_code not found'},
+            status=status.HTTP_404_NOT_FOUND
         )
