f7c74ccd39741ea6146ec89c3238f5e3305f8bda
Pratheeksha Dhananjaya <pratheekshadhananjaya@Mac.fritz.box>
2026-05-15 19:58:48 +0200

    updated GET endpoint

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [ path('api/v1/short-codes' , views.shorten_url, name='shorten_url')]
+urlpatterns = [ path('api/v1/short-codes' , views.shorten_url, name='shorten_url'),
+               path('api/v1/short-codes/<str:short_code>',views.redirect_url, name='redirect_url')]
--- shortener/views.py
+++ shortener/views.py
@@ -2,8 +2,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
+from django.http import HttpResponsePermanentRedirect
+from django.shortcuts import get_object_or_404
 from .models import Url
 from .utils import generate_short_code
+from urllib.parse import urlparse
 
 @api_view(['POST'])
 def shorten_url(request):
@@ -15,6 +18,25 @@ def shorten_url(request):
             status = status.HTTP_400_BAD_REQUEST
         )
     
+    try:
+        parsed = urlparse(long_url)
+        if parsed.scheme not in ['http' , 'https']:
+            return Response(
+                {'message': 'URL must start with http:// or https://'},
+                status = status.HTTP_400_BAD_REQUEST
+            )
+        if not parsed.netloc:
+            return Response(
+                {'message' : 'Please enter a valid URL'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    except Exception:
+        return Response(
+            {'message': 'Please enter a valid URL'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+       
+    
     short_code = generate_short_code()
     url = Url.objects.create(
         long_url = long_url,
@@ -22,7 +44,18 @@ def shorten_url(request):
     )
 
     return Response({
-        'shortUrl' : f'http://localhost:8000/{short_code}',
+        'shortUrl' : f'http://127.0.0.1:8000/api/v1/short-codes/{short_code}',
         'longUrl' : long_url,
         'createdAt' : url.created_at
     }, status = status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def redirect_url(request,short_code):
+    try:
+        url = Url.objects.get(short_code=short_code)
+        return HttpResponsePermanentRedirect(url.long_url)
+    except Url.DoesNotExist:
+        return Response(
+            {'message' : 'Short code not found'},
+            status = status.HTTP_404_NOT_FOUND
+        )
